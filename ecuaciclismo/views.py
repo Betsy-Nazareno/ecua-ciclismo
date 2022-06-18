bc3521e6c25a0a5cf885a3fe902c991be7440798
Betsy-Nazareno <belinaza@espol.edu.ec>
2022-06-18 02:59:12 -0500

    .

--- ecuaciclismo/views.py
+++ ecuaciclismo/views.py
@@ -2,6 +2,8 @@ import git
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+import logging
+
 @csrf_exempt
 def update(request):
     if request.method == "POST":
@@ -10,13 +12,14 @@ def update(request):
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
+        logging.basicConfig(level=logging.INFO)
         
-        # repo = git.Repo("ecua-ciclismo/")
-        # origin = repo.remotes.origin
-        # origin.pull()
+        repo = git.Repo("ecua-ciclismo/")
+        origin = repo.remotes.origin
+        origin.pull()
 
-        g = git.Git('ecua-ciclismo/')
-        g.pull('origin','branch-name')
+        # g = git.Git('ecua-ciclismo/')
+        # g.pull('origin','branch-name')
        
         return HttpResponse("Updated code on PythonAnywhere")
     else:
