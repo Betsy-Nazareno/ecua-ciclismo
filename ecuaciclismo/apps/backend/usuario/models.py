4511d2b2ff0b6836a1cd3f925aacc5976011709c
Enrique Yugcha <esyugcha@espol.edu.ec>
2022-06-21 00:01:40 -0500

    fix(consejo del dia): se agregan nuevos modelos para el usuario

--- ecuaciclismo/apps/backend/usuario/models.py
+++ ecuaciclismo/apps/backend/usuario/models.py
@@ -3,8 +3,26 @@ from django.db import models
 
 from ecuaciclismo.helpers.models import ModeloBase
 
+class Bicicleta(ModeloBase):
+    marca = models.CharField(max_length=20)
+    modelo = models.CharField(max_length=50)
+    anio = models.IntegerField(null=True)
+    estado = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=20)
+    foto_bicicleta = models.TextField()
+
 class DetalleUsuario(ModeloBase):
     usuario = models.OneToOneField(User, on_delete=models.PROTECT)
+    celular = models.CharField(max_length=10, null=True)
+    fecha_nacimiento = models.DateField(null=True)
+    genero = models.CharField(max_length=15, null=True)
+    nivel = models.IntegerField(default=0)
+    foto = models.TextField(null=True)
+    admin = models.BooleanField(default=False)
 
     def __init__(self, *args, **kwargs):
         super(DetalleUsuario, self).__init__(*args, **kwargs)
+
+class DetalleBicicleta(ModeloBase):
+    usuario = models.ForeignKey(User, on_delete=models.PROTECT)
+    bicicleta = models.ForeignKey(Bicicleta, on_delete=models.PROTECT)
