5dffb541cf3199c0ae66bad09ad1c292aada65d1
Enrique Yugcha <esyugcha@espol.edu.ec>
2022-06-25 02:45:58 -0500

    fix(recuperar contraseña): configuración para enviar el correo

--- ecuaciclismo/settings_win.py
+++ ecuaciclismo/settings_win.py
@@ -180,3 +180,20 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+SERVER_EMAIL = 'notificaciones.comextweb@gmail.com'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_SUBJECT_PREFIX = '[Aranceles-DEV]'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'notificaciones.comextweb@gmail.com'
+EMAIL_HOST_PASSWORD = 'dqffhfxcsbgrhnoh'
+
+ENVIAR_NOTIFICACIONES_EMAIL_GLOBAL = True
+
+URL = '127.0.0.1' #PONER URL DEL FRONT
+URLC = '127.0.0.1:8000' #PONER URL DEL FRONT
+HTTP = 'http://'
