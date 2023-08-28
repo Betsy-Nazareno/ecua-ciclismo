06fa3f0b3d3bfdf14d55bcda76427d3b3b7a6c9e
MRegit <moaaatup@espol.edu.ec>
2023-08-28 12:09:39 -0500

    fix(Solicitud): Actualizar estado de una solicitud

--- ecuaciclismo/apps/backend/api/solicitud/views.py
+++ ecuaciclismo/apps/backend/api/solicitud/views.py
@@ -6,6 +6,7 @@ from rest_framework import viewsets
 from django.contrib.auth.models import User
 from rest_framework.decorators import action
 from ecuaciclismo.apps.backend.lugar.models import Lugar
+from ecuaciclismo.apps.backend.ruta.models import Coordenada, Ubicacion
 from ecuaciclismo.apps.backend.solicitud.models import Solicitud, SolicitudLugar, SolicitudVerificado
 from ecuaciclismo.helpers.tools_utilities import ApplicationError, get_or_none
 from rest_framework.authtoken.models import Token
@@ -98,6 +99,21 @@ class SolicitudViewSet(viewsets.ModelViewSet):
                     solicitud['direccion']=datoLugar[0]['direccion']
                     solicitud['descripcion']=datoLugar[0]['descripcion']
                     solicitud['imagen']=datoLugar[0]['imagen']
+                    ubicacion = get_or_none(Ubicacion, id=datoLugar[0]['ubicacion_id'])
+                    coordenada_x = get_or_none(Coordenada, id=ubicacion.coordenada_x_id)
+                    coordenada_y = get_or_none(Coordenada, id=ubicacion.coordenada_y_id)
+                    dicc = {
+                        "coordinateX": {
+                            "latitude": float(coordenada_x.latitud),
+                            "longitude": float(coordenada_x.longitud)
+                        },
+                        "coordinateY": {
+                            "latitude": float(coordenada_y.latitud),
+                            "longitude": float(coordenada_y.longitud)
+                        }
+                    }
+                    solicitud['ubicacion'] = dicc
+
 
                     if solicitud.get('path_Pdf') is not None:
                         solicitud['tipo']="Registro Local"
@@ -111,6 +127,9 @@ class SolicitudViewSet(viewsets.ModelViewSet):
                     solicitud['nombre']="Solicitud Verificación"
                     solicitud['tipo']="Verificacion"
                     solicitud['descripcion']=solicitudVerificado.descripcion
+                    solicitud['imagen']=solicitudVerificado.imagen
+                    solicitud['usuarios']= SolicitudVerificado.get_Usuarios(solicitud['id'])
+                    
     
                 
             return jsonx({'status': 'success', 'message': 'Solicitudes obtenidas con éxito','solicitudes':solicitudes})
@@ -123,39 +142,17 @@ class SolicitudViewSet(viewsets.ModelViewSet):
             transaction.rollback()
             return jsonx(False, str(e))
     
-    @action(detail=False,url_path='get_solicitudes_user',methods=['get'])
-    def get_solicitudes_user(self,request):
+    @action(detail=False,url_path='responder_solicitud',methods=['post'])
+    def responder_solicitud(self,request):
+        transaction.set_autocommit(False)
         try:
-            token = Token.objects.get(key=request.headers['Authorization'].split('Token ')[1])
-            solicitudes= Solicitud.get_by_user(token.user.id)
-
-            for solicitud in solicitudes:
-                solicitud['fecha_creacion']=datetime.strftime(solicitud['fecha_creacion'],'%Y-%m-%d %H:%M:%S')
-                solicitud['fecha_creacion']=str(solicitud['fecha_creacion'])
-                solicitudLugar=get_or_none(SolicitudLugar, id=solicitud['id'])
-                if solicitudLugar is not None:
-                    solicitud['tipo']="Recomendados"
-                    datoLugar=SolicitudLugar.get_by_id(solicitud['id'])
-                    solicitud['nombre']=datoLugar[0]['nombre']
-                    solicitud['direccion']=datoLugar[0]['direccion']
-                    solicitud['descripcion']=datoLugar[0]['descripcion']
-                    solicitud['imagen']=datoLugar[0]['imagen']
-
-                    if solicitud.get('path_Pdf') is not None:
-                        solicitud['tipo']="Registro Local"
-                
-                else:
-                    solicitud['nombre']="Solicitud Membresia"
-                    solicitud['tipo']="Membresia"
-                
-                solicitudVerificado=get_or_none(SolicitudVerificado, id=solicitud['id'])
-                if solicitudVerificado is not None:
-                    solicitud['nombre']="Solicitud Verificación"
-                    solicitud['tipo']="Verificacion"
-                    solicitud['descripcion']=solicitudVerificado.descripcion
-                    solicitud['fotUser']=solicitudVerificado.user.foto
-                
-            return jsonx({'status': 'success', 'message': 'Solicitudes obtenidas con éxito','solicitudes':solicitudes})
+            data = request.data
+            solicitud = Solicitud.objects.get(id=data['token_solicitud'])
+            solicitud.estado=data['estado']
+            solicitud.motivo_rechazo=data['motivo_rechazo']
+            solicitud.save()
+            transaction.commit()
+            return jsonx({'status': 'success', 'message': 'Solicitud respondida con éxito'})
         except ValidationError as e:
             return jsonx({'status': 'error', 'message': e.message_dict})
         except ApplicationError as msg:
--- ecuaciclismo/apps/backend/solicitud/models.py
+++ ecuaciclismo/apps/backend/solicitud/models.py
@@ -14,9 +14,10 @@ class Solicitud(ModeloBase):
     def get_all(cls):
         cursor = connection.cursor()
         sql = '''
-                SELECT user.first_name, user.last_name, detalle_usuario.foto, solicitud.token, solicitud.estado, solicitud.fecha_creacion,solicitud.path_Pdf, solicitud.id
+                SELECT user.first_name, user.last_name,token.key as token_usuario,detalle_usuario.foto, solicitud.token, solicitud.estado, solicitud.fecha_creacion,solicitud.path_Pdf, solicitud.id, solicitud.motivo_rechazo
                 FROM `solicitud_solicitud` AS solicitud
                 LEFT JOIN `auth_user` AS user ON solicitud.user_id = user.id
+                LEFT JOIN `authtoken_token` AS token ON solicitud.user_id = token.user_id
                 LEFT JOIN `usuario_detalleusuario` AS detalle_usuario ON solicitud.user_id = detalle_usuario.usuario_id
                 ORDER BY solicitud.fecha_creacion 
             '''
@@ -31,15 +32,15 @@ class Solicitud(ModeloBase):
         return dic
     
     @classmethod
-    def get_by_user(cls, id):
+    def get_by_token(cls, token):
         cursor = connection.cursor()
         sql = '''
-                SELECT user.first_name, user.last_name, detalle_usuario.foto, solicitud.token, solicitud.estado, solicitud.fecha_creacion,solicitud.path_Pdf, solicitud.id
+                SELECT user.first_name, user.last_name,token.key as token_usuario, detalle_usuario.foto, solicitud.token, solicitud.estado, solicitud.fecha_creacion,solicitud.path_Pdf, solicitud.id
                 FROM `solicitud_solicitud` AS solicitud
                 LEFT JOIN `auth_user` AS user ON solicitud.user_id = user.id
+                LEFT JOIN `authtoken_token` AS token ON solicitud.user_id = token.user_id
                 LEFT JOIN `usuario_detalleusuario` AS detalle_usuario ON solicitud.user_id = detalle_usuario.usuario_id
-                WHERE solicitud.user_id = ''' + str(id) + '''
-                ORDER BY solicitud.fecha_creacion 
+                WHERE solicitud.id = ''' + str(token) + '''
             '''
         cursor.execute(sql)
         dic = []
@@ -85,7 +86,7 @@ class SolicitudVerificado(Solicitud):
     def get_Usuarios(self, id):
         cursor = connection.cursor()
         sql = '''
-                SELECT user.first_name, user.last_name, detalle_usuario.foto, user.id
+                SELECT user.first_name, user.last_name, detalle_usuario.foto, detalle_usuario.tipo, user.id
                 FROM `solicitud_solicitudverificado_usuarios` AS solicitud_usuario
                 LEFT JOIN `auth_user` AS user ON solicitud_usuario.user_id = user.id
                 LEFT JOIN `usuario_detalleusuario` AS detalle_usuario ON user.id = detalle_usuario.usuario_id
