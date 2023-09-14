37966c9d2c5db5f7317ce52935515a62831951f0
MRegit <moaaatup@espol.edu.ec>
2023-09-14 12:57:38 -0500

    fix(solicitud): devuelve token:notificacion

--- ecuaciclismo/apps/backend/solicitud/models.py
+++ ecuaciclismo/apps/backend/solicitud/models.py
@@ -14,7 +14,7 @@ class Solicitud(ModeloBase):
     def get_all(cls):
         cursor = connection.cursor()
         sql = '''
-                SELECT user.first_name, user.last_name,token.key as token_usuario,detalle_usuario.foto, solicitud.token, solicitud.estado, solicitud.fecha_creacion,solicitud.path_Pdf, solicitud.id, solicitud.motivo_rechazo
+                SELECT user.first_name, user.last_name,token.key as token_usuario,detalle_usuario.foto, detalle_usuario.token_notificacion, solicitud.token, solicitud.estado, solicitud.fecha_creacion,solicitud.path_Pdf, solicitud.id, solicitud.motivo_rechazo
                 FROM `solicitud_solicitud` AS solicitud
                 LEFT JOIN `auth_user` AS user ON solicitud.user_id = user.id
                 LEFT JOIN `authtoken_token` AS token ON solicitud.user_id = token.user_id
