0af39f52ffbe39798b7a54f10bb6b2674b3ee2ce
Methi <edumbrav@espol.edu.ec>
2024-01-12 21:25:50 -0500

    changes

--- ecuaciclismo/apps/backend/bicicleta/models.py
+++ ecuaciclismo/apps/backend/bicicleta/models.py
@@ -3,12 +3,16 @@ from django.contrib.auth.models import User
 
 class Bicicleta(models.Model):
 
-    tipo = models.CharField(max_length=50, blank=True)
-    marca = models.CharField(max_length=100, blank=True)
-    codigo = models.CharField(max_length=50, blank=True)
-  
+    modelo = models.CharField(max_length=500, blank=True)
+    marca = models.CharField(max_length=500, blank=True)
+    codigo = models.CharField(max_length=500, blank=True)
+    modadlidad = models.CharField(max_length=500, blank=True)
+    n_serie= models.CharField(max_length=500, blank=True)
+    tienda_origen = models.CharField(max_length=500, blank=True)
+    factura = models.CharField(max_length=500, blank=True)
+    color = models.CharField(max_length=500, blank=True)
     def __str__(self):
-        return f"{self.tipo} - {self.marca} - {self.codigo}"
+        return f"{self.modelo} - {self.marca} - {self.codigo} - {self.modadlidad} - {self.n_serie} - {self.tienda_origen} - {self.factura} - {self.color}"
 
 class ImagenBicicleta(models.Model):
     imagen_url = models.CharField(max_length=200,null=True)
