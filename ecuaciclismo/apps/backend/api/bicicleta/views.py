cb45bed4c1d05b2fa29c81724207732b59d0d354
Methi <edumbrav@espol.edu.ec>
2024-01-14 23:09:59 -0500

    a

--- ecuaciclismo/apps/backend/api/bicicleta/views.py
+++ ecuaciclismo/apps/backend/api/bicicleta/views.py
@@ -99,7 +99,7 @@ class BicicletaViewSet(viewsets.ModelViewSet):
             if detalle_usuario.admin == 0:
                 return jsonx({'status': 'error', 'message': 'No tiene permiso para realizar esta acción.'})
             
-            token_usuario = Token.objects.get(token=data['token_usuario'])
+            token_usuario = Token.objects.get(key=request.data['token_usuario'])
             print(token_usuario)
             bicicletas_usuario = PropietarioBicicleta.objects.filter(usuario=token_usuario.user).select_related('bicicleta')
             bicicletas = [prop.bicicleta for prop in bicicletas_usuario]
