9cfca3064634014b47ed549b80c3f8d989a2230d
kevcorti <kevcorti@espol.edu.ec>
2024-12-08 18:37:15 -0500

    Cambios en el backend

--- ecuaciclismo/apps/safepoint/urls.py
+++ ecuaciclismo/apps/safepoint/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('autenticacion/', include('ecuaciclismo.apps.safepoint.autenticacion.urls')),
-    path('negocio/', include('ecuaciclismo.apps.safepoint.negocio.urls'))
+    path('negocio/', include('ecuaciclismo.apps.safepoint.negocio.urls')) 
 ]
